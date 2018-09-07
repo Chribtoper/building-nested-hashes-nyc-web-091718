e3093b179d057a1808bf019be8cbfce7bfe85ad2
Chribtoper <xcstolfax@gmail.com>
2018-09-07 22:27:57 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,6 +21,7 @@ def bonus
   }
 
   #code your solution here:
+def edit
 bonus.epic_tragedy[:montague][:hero][:status] = "dead"
 bonus.epic_tragedy[:capulet][:heroine][:status] = "dead"
 puts bonus.epic_tragedy
